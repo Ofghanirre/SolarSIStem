aeadcf6aa94d4759ad9f190bd9faba3575a60f35
collet <etienne.collet@globalis-ms.com>
2023-12-29 11:00:09 +0100

    trying to fix the drawing refactor

--- src/shared/planets.hpp
+++ src/shared/planets.hpp
@@ -61,15 +61,9 @@ struct AStellarObject {
         GLuint vao, 
         Sphere sphere
     ) {
-<<<<<<< Updated upstream
         glm::mat4 matrixPos = draw(globalMVMatrix, viewMatrix, ProjMatrix, time, vao, sphere);
         for(uint i = 0; i < m_satellites.size(); i++){
             std::cout << "New sattelites" << std::endl;
-=======
-        std::cout << initial_position << std::endl;
-        glm::mat4 matrixPos = draw(globalMVMatrix, viewMatrix, ProjMatrix, time, vao, sphere, initial_position, rotation_axis);
-        for(uint i = 0; i < m_satellites.size(); i++){            
->>>>>>> Stashed changes
             m_satellites[i]->drawAll(matrixPos, viewMatrix, ProjMatrix, time, vao, sphere);
         }
     }
@@ -180,59 +174,7 @@ struct SunProgram : public AStellarObject{
         glBindVertexArray(0); // On utilise l'array vao
         return globalMVMatrix * viewMatrix;
     }
-
-    glm::mat4 getPosMatrix(glm::mat4 globalMVMatrix, float time) {
-        return globalMVMatrix;
-    }
 };
-
-
-struct MercureProgram : public AStellarObject {
-    const float coef_diametre = 0.02439;
-    const float dist_sol = 2.06;
-    glm::vec3 sattelites_rotation_axis = glm::vec3(0, 1, 0); //glm::sphericalRand(1.f);
-    glm::vec3 sattelites_initial_position = glm::vec3(dist_sol, 0, 0); //glm::sphericalRand(2.f);
-
-    MercureProgram(Program& program, std::vector<const GLchar*> textures_uniform_locations, std::vector<GLuint> texturesIds):
-            AStellarObject {program, textures_uniform_locations, texturesIds}
-    {}
-
-    glm::mat4 draw(
-            glm::mat4 globalMVMatrix,
-            glm::mat4 viewMatrix,
-            glm::mat4 ProjMatrix,
-            float time,
-            GLuint vao,
-            Sphere sphere) override
-    {
-        use();
-        for(uint i = 0; i < AStellarObject::m_texturesIds.size(); i++){
-            glUniform1i(AStellarObject::m_textures[i], i);
-        }
-
-        glm::mat4 MVMatrixPlanetView = globalMVMatrix * viewMatrix;
-        glm::mat4 sunMVMatrix = glm::rotate(MVMatrixPlanetView, time, glm::vec3(0, 1, 0));
-        sunMVMatrix = glm::scale(sunMVMatrix, glm::vec3(coef_diametre, coef_diametre, coef_diametre));
-        glUniformMatrix4fv(AStellarObject::m_uMVMatrix, 1, GL_FALSE,
-                           glm::value_ptr(sunMVMatrix));
-        glUniformMatrix4fv(AStellarObject::m_uNormalMatrix, 1, GL_FALSE,
-                           glm::value_ptr(glm::transpose(glm::inverse(sunMVMatrix))));
-        glUniformMatrix4fv(AStellarObject::m_uMVPMatrix, 1, GL_FALSE,
-                           glm::value_ptr(ProjMatrix * sunMVMatrix));
-
-        for(uint i = 0; i < AStellarObject::m_texturesIds.size(); i++){
-            glActiveTexture(AStellarObject::ArchiveTextureName[i]);
-            glBindTexture(GL_TEXTURE_2D, AStellarObject::m_texturesIds[i]);
-        }
-
-        glBindVertexArray(vao); // On utilise l'array vao
-        glDrawArrays(GL_TRIANGLES, 0, sphere.getVertexCount());
-        glBindTexture(GL_TEXTURE_2D, 0);
-        glBindVertexArray(0); // On utilise l'array vao
-        return globalMVMatrix * viewMatrix;
-    }
-};
-
 
 struct MercureProgram : public AStellarObject {
     const float COEF_DIAMETRE = 0.02439;
