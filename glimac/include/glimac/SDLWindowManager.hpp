d6fcc14629530e5025ed8758142c1d8e5e36a992
Etienne_C <etienne.collet@globalis-ms.com>
2024-01-02 09:10:09 +0100

    Bugfix - delete memory leak

--- glimac/include/glimac/SDLWindowManager.hpp
+++ glimac/include/glimac/SDLWindowManager.hpp
@@ -3,19 +3,11 @@
 #include <cstdint>
 #include <SDL/SDL.h>
 #include "glm.hpp"
-/*
-#include <SDL/SDL_image.h>
-#include <SDL/SDL_ttf.h>
-*/
 
 namespace glimac {
 
 class SDLWindowManager {
 public:
-    /*SDL_Surface *screen;
-    SDL_Color fColor;
-    SDL_Rect fontRect;
-    TTF_Font* font;*/
     SDLWindowManager(uint32_t width, uint32_t height, const char* title);
 
     ~SDLWindowManager();
@@ -34,7 +26,6 @@ public:
     // Return the time in seconds
     float getTime() const;
 
-    //void printF(char *c, int x, int y);
 };
 
 }
